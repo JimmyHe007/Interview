b0471e4f28a04da3e98aaff0d4e5be65e8997e46
JimmyHe97 <he9527@outlook.com>
2020-02-19 12:21:55 +0800

    添加算法基础结构题

---
+++ src/main/java/Algorithm/unit1/DailyTemperatures.java
@@ -0,0 +1,70 @@
+package Algorithm.unit1;
+
+import java.util.Stack;
+
+/**
+ * 739. 每日温度
+ */
+public class DailyTemperatures {
+
+    public static void main(String[] args) {
+
+        int[] temperatures = {55,38,53,81,61,93,97,32,43,78};
+        int[] result = dailyTemperatures(temperatures);
+        for (int i : result) {
+            System.out.print(i);
+        }
+
+    }
+
+    public static int[] dailyTemperatures(int[] T) {
+
+        int[] result = new int[T.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = T.length - 1; i >= 0; --i) {
+            while (!stack.isEmpty() && T[i] >= T[stack.peek()]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+            stack.push(i);
+        }
+        return result;
+    }
+
+//    public static int[] dailyTemperatures(int[] T) {
+//
+//        int[] result = new int[T.length];
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(0);
+//        for (int i = 1; i < T.length; i++) {
+//            int prev = stack.pop();
+//            int current = T[i];
+//            int j = i;
+//            while (current <= T[prev]) {
+//                if (j == i) {
+//                    stack.push(prev);
+//                }
+//                stack.push(j);
+//                j++;
+//                if (!(j < T.length)) {
+//                    j = prev;
+//                    stack.clear();
+//                    break;
+//                }
+//                current = T[j];
+//            }
+//            while (current > T[prev]) {
+//                if (stack.isEmpty()) {
+//                    break;
+//                } else {
+//                    prev = stack.pop();
+//                }
+//            }
+//
+//            result[i-1] = j-prev;
+//            stack.push(i);
+//        }
+//        return result;
+//    }
+
+}
---
+++ src/main/java/Algorithm/unit1/ValidAnagram.java
@@ -0,0 +1,70 @@
+package Algorithm.unit1;
+
+
+/**
+ * 248. 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ */
+public class ValidAnagram {
+
+    public static void main(String[] args) {
+
+        String s = "anagram";
+        String t = "nagaram";
+        boolean result = compare(s, t);
+        System.out.println(result);
+
+    }
+
+    public static boolean compare(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] letter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            letter[s.charAt(i) - 'a'] ++;
+            letter[t.charAt(i) - 'a'] --;
+        }
+        for (int i : letter) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+//    public static boolean compare(String s, String t) {
+//        if (s.length() != t.length()) {
+//            return false;
+//        }
+//        char[] schar = s.toCharArray();
+//        char[] tchar = t.toCharArray();
+//        Map<Character, Integer> distinct = new HashMap<>();
+//        for (char c : schar) {
+//            if (distinct.containsKey(c)) {
+//                distinct.put(c, distinct.get(c)+1);
+//            } else {
+//                distinct.put(c, 1);
+//            }
+//        }
+//        for (char c : tchar) {
+//            if (distinct.containsKey(c)) {
+//                if (distinct.get(c) > 0) {
+//                    if (distinct.get(c) <= 1) {
+//                        distinct.remove(c);
+//                    } else {
+//                        distinct.put(c, distinct.get(c)-1);
+//                    }
+//                }
+//            } else {
+//                return false;
+//            }
+//        }
+//        if (distinct.size() > 0) {
+//            return false;
+//        } else {
+//            return true;
+//        }
+//    }
+
+
+}
